22753855b4c2e7eb1f4af3334b8bc58d81552a01
kovkinvladyslav <kovkinvlad@gmail.com>
2025-04-11 15:37:31 +0300

    add model to the controller

--- js/AppMain.js
+++ js/AppMain.js
@@ -1,5 +1,7 @@
 import Controller from './controller/AppController.js'
 import View from './view/AppView.js'
+import Model from './model/AppModel.js'
 
 let appView = new View()
-let controller = new Controller(appView)
+let appModel = new Model()
+let controller = new Controller(appView, appModel)
